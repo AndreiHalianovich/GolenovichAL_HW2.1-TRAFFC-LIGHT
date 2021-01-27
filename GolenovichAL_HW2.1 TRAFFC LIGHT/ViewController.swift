663d14d2e0f4ae0f64fc43404b398f6bf5628ed4
Andrei Halianovich <igolenovichbox@gmail.com>
2021-01-27 19:15:46 +0300

    Initial Commit

---
+++ GolenovichAL_HW2.1 TRAFFC LIGHT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GolenovichAL_HW2.1 TRAFFC LIGHT
+//
+//  Created by Andrei Halianovich on 27.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
