027c80fd1aa0e3488e92d0dc832dc0ed7244b5f9
Andrei Halianovich <igolenovichbox@gmail.com>
2021-01-27 23:15:25 +0300

    add all func

--- GolenovichAL_HW2.1 TRAFFC LIGHT/ViewController.swift
+++ GolenovichAL_HW2.1 TRAFFC LIGHT/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet var trafficLightsArray: [UIView]!
+    @IBOutlet var startButton: UIButton!
+    var click = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for light in trafficLightsArray {
+            light.layer.cornerRadius = light.frame.height / 2
+        }
+        startButton.layer.cornerRadius = startButton.frame.height / 4
     }
-
-
+    
+    @IBAction func startButtonDidClick(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        switch click {
+        case 0:
+            trafficLightsArray[click+2].alpha = 0.3
+            trafficLightsArray[click].alpha = 1
+            click += 1
+        case 1:
+            trafficLightsArray[click - 1].alpha = 0.3
+            trafficLightsArray[click].alpha = 1
+            click += 1
+        case 2:
+            trafficLightsArray[click - 1].alpha = 0.3
+            trafficLightsArray[click].alpha = 1
+            click = 0
+        default:
+            break
+        }
+    }
 }
 
